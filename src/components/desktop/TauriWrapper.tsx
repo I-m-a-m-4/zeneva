57c6ae23051b5bffeb5eb4ba9e244a4a9c6d53e6
i-m-a-m-4 <belloimam431@gmail.com>
2026-04-28 14:06:39 +0100

    Auto-deploy: Fix marketing page scrolling

--- src/components/desktop/TauriWrapper.tsx
+++ src/components/desktop/TauriWrapper.tsx
@@ -14,12 +14,18 @@ export function TauriLayoutWrapper({ children }: { children: React.ReactNode }) 
 
   return (
     <div 
-      className="h-screen bg-background text-foreground flex flex-col overflow-hidden"
+      className={cn(
+        "bg-background text-foreground flex flex-col",
+        isTauri ? "h-screen overflow-hidden" : "min-h-screen"
+      )}
       style={{ '--tauri-title-height': isTauri ? '2.25rem' : '0px' } as React.CSSProperties}
     >
       {/* Spacer for Tauri TitleBar (h-9 = 2.25rem) */}
       {isTauri && <div className="h-9 w-full shrink-0" />}
-      <div className="flex-1 flex flex-col relative min-h-0">
+      <div className={cn(
+        "flex-1 flex flex-col relative",
+        isTauri && "min-h-0"
+      )}>
         {children}
       </div>
     </div>
